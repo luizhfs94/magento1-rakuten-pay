27338a2587d782541a2e87ac255b734323dbc7ae
Alex <alexsilva.info@gmail.com>
2018-10-01 14:29:18 -0300

    updated license

--- app/code/community/Rakuten/RakutenLogistics/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/community/Rakuten/RakutenLogistics/Block/Adminhtml/Sales/Order/Grid.php
@@ -2,7 +2,7 @@
  
 /**
  ************************************************************************
- * Copyright [2018] [RakutenLogistics]
+ * Copyright [2018] [RakutenConnector]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
